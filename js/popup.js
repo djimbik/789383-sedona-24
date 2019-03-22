1a92ee37aa5d0b1f6d98749fe9fbfb54fcad3e9c
djimbik <ringoallen93@gmail.com>
2019-03-22 22:44:58 +0300

    сделал JS

---
+++ js/popup.js
@@ -0,0 +1,36 @@
+var link = document.querySelector(".search__open-button");
+var popup = document.querySelector(".search__form--off");
+
+var form = document.querySelector("form");
+var dateIn = popup.querySelector("[name=date-in]");
+var dateOut = popup.querySelector("[name=date-out]");
+var adults = popup.querySelector("[name=adults]");
+var children = popup.querySelector("[name=children]");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("adults");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+link.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.toggle("search__form");
+});
+
+form.addEventListener("submit", function(evt) {
+if (!dateIn.value || !dateOut.value || !adults.value || !children.value) {
+  evt.preventDefault();
+  popup.classList.remove("search__form--error");
+  popup.offsetWidth = popup.offsetWidth;
+  popup.classList.add("search__form--error");
+  } else {
+      if (isStorageSupport) {
+        localStorage.setItem("adults", adults.value);
+        localStorage.setItem("children", children.value);
+      }
+  }
+});
